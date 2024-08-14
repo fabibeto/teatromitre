a2b34f264b38295f9aa87554aa95374011b8f558
fabi_beto <fabi_beto@yahoo.com.ar>
2024-08-14 18:46:26 -0300

    Mi primera version del Teatro Mitre

---
+++ data.js
@@ -0,0 +1,138 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-foto_1",
+      "name": "foto_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 1.8866729210229396,
+        "pitch": 0,
+        "fov": 1.1644744523456731
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0770398942961581,
+          "pitch": 0.0112364506698448,
+          "rotation": 0,
+          "target": "1-foto_2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3047276599888029,
+          "pitch": -0.0037419663269417924,
+          "title": "Agosto en el Mitre",
+          "text": "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;14 de Agosto-16hs<br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Feria del Libro<br>Disparate - Historias de Maria Elena Walsh a cargo de Cristina Iriarte"
+        }
+      ]
+    },
+    {
+      "id": "1-foto_2",
+      "name": "foto_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.120288136704115,
+          "pitch": 0.10008504381332806,
+          "rotation": 0,
+          "target": "0-foto_1"
+        },
+        {
+          "yaw": 0.01728276004068796,
+          "pitch": 0.09211651452311997,
+          "rotation": 0,
+          "target": "2-foto_3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-foto_3",
+      "name": "foto_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -0.005776241166680407,
+        "pitch": 0.004040829569383675,
+        "fov": 1.1644744523456731
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05463030761425891,
+          "pitch": 0.07107021047749207,
+          "rotation": 6.283185307179586,
+          "target": "1-foto_2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Teatro Mitre",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
